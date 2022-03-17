3b52578e4fda4088bbdc77332b0f50aceb7bfe4b
JosferLV <79889017+JosferLV@users.noreply.github.com>
2022-03-17 15:23:19 -0600

    Update DANIELCHAVEZGALLARDO.php

--- DANIELCHAVEZGALLARDO.php
+++ DANIELCHAVEZGALLARDO.php
@@ -2,7 +2,7 @@
 
     <body stlye="background: linear-gradient(to right, #182848, #4b6cb7);">
         <head>
-            <h1 class="center" >EDUARDO MENDIETA LOZANO</h1>
+            <h1 class="center" >DANIEL CHAVEZ GALLARDO</h1>
         </head>
     </body>
 </html>

82a74875d09a8f2c9124e40efc33b8e262263a54
requiem59 <94019721+requiem59@users.noreply.github.com>
2022-03-23 20:16:28 -0600

    Create CHRISTIANMEJIAMONDRAGON.php

---
+++ CHRISTIANMEJIAMONDRAGON.php
@@ -0,0 +1,9 @@
+<?php include ("header.php"); ?>
+
+    <body stlye="background: linear-gradient(to right, #CA0000, #780101);">
+        <head>
+            <h1 class="center" >CHRISTIAN ALEXIS MEJÍA MONDRAGÓN</h1>
+        </head>
+    </body>
+
+</html>

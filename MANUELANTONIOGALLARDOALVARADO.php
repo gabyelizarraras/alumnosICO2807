e78325b4407595cc9b8d921338e865baec5a3ff5
manuwin <101827297+manuwin@users.noreply.github.com>
2022-03-24 18:37:59 -0600

    Manuel añadió php

---
+++ MANUELANTONIOGALLARDOALVARADO.php
@@ -0,0 +1,8 @@
+<?php include ("header.php"); ?>
+
+    <body stlye="background: linear-gradient(to right, #182848, #4b6cb7);">
+        <head>
+            <h1 class="center" >MANUEL ANTONIO GALLARDO ALVARADO</h1>
+        </head>
+    </body>
+</html>
